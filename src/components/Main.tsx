4a337e43d73a235cd92bc2e2c93c1e5ceea8297e
JAY KUMAR <jaykumar@cdac.in>
2026-01-20 09:37:04 +0530

    test

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -16,7 +16,7 @@ function Main() {
             <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
             <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
-          <h1>Jay Kumar Pathak</h1>
+          <h1>Jay Kumar</h1>
           <p>Full Stack Engineer</p>
 
           <div className="mobile_social_icons">
